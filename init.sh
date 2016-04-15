14d159a7be1dc7cdba0dac3500dea9a788ae502b
micaheljo94 <michaeljohann94@outlook.com>
2016-04-15 17:42:28 +0200

    runtime optimize; added chsh -s /bin/zsh

--- init.sh
+++ init.sh
@@ -5,38 +5,47 @@
 # michaeljohann94@outlook.com
 #
 ##
-
 #Syncs with github if needed
 
 #rc Dotfiles
 if [ ! -d $HOME/dotfile ]; then
-git clone https://github.com/michaeljo94/dotfile.git ~/dotfile
+    printf "Downloading dotfile\n"
+    git clone https://github.com/michaeljo94/dotfile.git ~/dotfile
 fi
 
 #Vundel for vim
 if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    printf "Downloading Vundle for Vim\n"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 #antigen for zsh
 if [ ! -e $HOME/antigen/antigen.zsh ]; then 
-git clone https://github.com/zsh-users/antigen.git ~/antigen
+    printf "Downloading antigen for ZSh\n"
+    git clone https://github.com/zsh-users/antigen.git ~/antigen
 fi
+
 
 #linking rc-files
-#tmux.conf
-if [ ! -e $HOME/.tmux.conf ]; then
-    ln -s $HOME/dotfile/.tmux.conf $HOME/.tmux.conf
+
+function safelink {
+   if [ ! -e $HOME/$1 ]; then
+       ln -s $HOME/dotfile/$1 $HOME/$1
+       echo $1 linked succesfully
+   else
+       echo $1 already exists
+   fi
+}
+
+printf "Trying to link RunControl\n\n"
+
+safelink .vimrc
+safelink .tmux.conf
+safelink .zshrc
+
+#change shell to zsh
+if [ $SHELL != "/bin/zsh" ]; then
+    chsh -s /bin/zsh
+  else
+    printf "\nyou are alread using ZSh\n\n"
 fi
-
-#zshrc
-if [ ! -e $HOME/.zshrc ]; then
-    ln -s $HOME/dotfile/.zshrc $HOME/.zshrc
-fi
-
-#vimrc
-if [ ! -e $HOME/.vim ]; then
-    ln -s $HOME/dotfile/.vimrc $HOME/.vimrc
-fi
-
-
