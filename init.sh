17fc8e37cb1da4f3ad6eca89abd4a9fc53a6e8cb
michaeljo94 <michaeljohann94@gmail.com>
2016-05-18 01:38:21 +0200

    expanded update funtion and new gitconfig

--- init.sh
+++ init.sh
@@ -27,10 +27,12 @@ function jogurt {
     
     cd $StartPath
     rm -rf /tmp/$USER-yaourt
+
 }
 
 function dotrepo {
 # Dotfile Repo
+
 if [ ! -d $HOME/dotfile ]; then
     printf "Downloading dotfile\n"
     git clone https://github.com/michaeljo94/dotfile.git $DotfilePath 
@@ -39,6 +41,7 @@ fi
 
 function vundle {
 #Vundel for vim
+
 if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
     printf "Downloading Vundle for Vim\n"
     git clone https://github.com/VundleVim/Vundle.vim.git $VundlePath
@@ -47,6 +50,7 @@ fi
 
 function tpm {
 #tmux plugin manager 
+
 if [ ! -d $HOME/.tmux/plugins/tpm ]; then
     printf "Downloading Tmux plugin Manager\n"
     git clone https://github.com/tmux-plugins/tpm $TpmPath
@@ -54,6 +58,7 @@ fi
 }
 
 function safelinkd {
+
     if [ ! -d $HOME/$2 ]; then
         ln -s $1 $HOME/$2
         printf "%s linked succesfully" "$(basename $1)"
@@ -63,6 +68,7 @@ function safelinkd {
 }
 
 function safelink {
+
    if [ ! -e $HOME/$1 ]; then
        ln -s $HOME/dotfile/$1 $HOME/$1
        printf "%s linked succesfully\n" "$1"
@@ -77,15 +83,25 @@ safelink .vimrc
 safelink .tmux.conf
 safelink .zshrc
 safelinkd /usr/lib/python3.5/site-packages/powerline .local/bin/powerline
-
 }
 
 function update () {
-    cd $1
-    printf "Checking %s ...\t" "$(basename $1)"
-    git pull
-    cd $StartPath
+     if [ -d $1 ]; then
+
+        cd $1
+        printf "NOTE: Checking %s ...\t" "$(basename $1)"
+        git pull
+        cd $StartPath
+
+    else
+        printf "%s does not exist" "$(basename $1)" "$2"
+    fi
 }
+
+if [[ $EUID -eq 0 ]]; then
+      echo "This script must NOT be run as root" 1>&2
+        exit 1
+    fi
 
 case $1 in
     "install" )
@@ -122,10 +138,23 @@ case $1 in
         esac
             ;;
     "update" )
-        dep
-        update $DotfilePath
-        update $VundlePath
-        update $TpmPath
+        case $2 in 
+            "all" )
+                update $DotfilePath
+                update $VundlePath
+                update $TpmPath
+                ;;
+            "vundle" )
+                update $VundlePath
+                ;;
+            "tpm" )
+                update $TpmPath
+                ;;
+            "yaourt" )
+                yaourt
+            ;;
+        esac
+
         ;;
     * )
         echo sounds like a bug not a feature
