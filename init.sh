e7bf02af0b439d5c712ad721e3bb118cffd07132
michaeljo94 <michaeljohann94@gmail.com>
2016-05-17 08:23:18 +0200

    added gitconfig and changed funtionality of the installer

--- init.sh
+++ init.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
+
+StartPath=$PWD
+VundlePath=$HOME/.vim/bundle/Vundle.vim
+TpmPath=$HOME/.tmux/plugins/tpm
+DotfilePath=$HOME/dotfile
+
+function repo {
+
 # Arch-linux packages
 if [ -e /bin/pacman ]; then
-    sudo pacman -S tmux vim zsh powerline powerline-fonts pydf wget curl
+    sudo pacman -Syu tmux vim zsh powerline powerline-fonts pydf wget curl --noconfirm 
 fi
 
 #Syncs with github if needed
 #rc Dotfiles
 if [ ! -d $HOME/dotfile ]; then
     printf "Downloading dotfile\n"
-    git clone https://github.com/michaeljo94/dotfile.git ~/dotfile
+    git clone https://github.com/michaeljo94/dotfile.git $DotfilePath 
 fi
 
 #Vundel for vim
 if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
     printf "Downloading Vundle for Vim\n"
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git $VundlePath
 fi
 
 #tmux plugin manager 
 if [ ! -d $HOME/.tmux/plugins/tpm ]; then
     printf "Downloading Tmux plugin Manager\n"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm $TpmPath
 fi
-
-#antigen for zsh
-if [ ! -e $HOME/antigen/antigen.zsh ]; then
-    printf "Downloading antigen for ZSh\n"
-    git clone https://github.com/zsh-users/antigen.git ~/antigen
-fi
+}
 #link powerline to ~/.local/bin
 function safelinkd {
     if [ ! -d $HOME/$2 ]; then
@@ -42,12 +45,15 @@ function safelinkd {
 function safelink {
    if [ ! -e $HOME/$1 ]; then
        ln -s $HOME/dotfile/$1 $HOME/$1
-       echo $1 linked succesfully
+       echo $1 linked succesfully\n
    else
-       echo $1 already exists
+       echo $1 already exists\m
    fi
 }
+function install {
 printf "Trying to link RunControl\n\n"
+
+safelink .gitconfig
 safelink .vimrc
 safelink .tmux.conf
 safelink .zshrc
@@ -57,5 +63,27 @@ safelinkd /usr/lib/python3.5/site-packages/powerline .local/bin/powerline
 if [ $SHELL != "/bin/zsh" ]; then
     chsh -s /bin/zsh
   else
-    printf "\nyou are alread using ZSh\n\n"
+    printf "you are alread using ZSh\n\n"
 fi
+}
+
+function update () {
+    cd $1
+    git pull
+    cd $StartPWD
+}
+
+if [ "$1" == "install" ]; then
+
+    repo
+    install
+    
+    elif [ "$1" == "update" ]; then
+
+        update $DotfilePath
+        update $VundlePath
+        update $TpmPath
+
+    else
+            echo "This is a feature not a bug"
+        fi
